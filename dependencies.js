fd85f2e5f963d6611ab60d3724927c50103ea3cc
Gabrielhj17 <gabrielhj@icloud.com>
2024-02-20 20:17:54 +0000

    made depedencies file

---
+++ dependencies.js
@@ -0,0 +1,8 @@
+// Import other JavaScript files
+document.write('<script src="game.js"></script>');
+document.write('<script src="characterLimit.js"></script>');
+document.write('<script src="moveCursor.js"></script>');
+document.write('<script src="rowEditing.js"></script>');
+document.write('<script src="rowCompleted.js"></script>');
+document.write('<script src="submitButton.js"></script>');
+document.write('<script src="updateBoxColours.js"></script>');
